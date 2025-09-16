3faebdc92044fa72c4e90ef8a1e5d17d379a3689
samarth sharma <samarths716@gmail.com>
2025-09-16 19:48:09 +0530

    lang

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -3,6 +3,7 @@ import { IoClose, IoMenuOutline } from 'react-icons/io5'
 import { navigationLInks } from './Header';
 import Link from 'next/link';
 import { Button } from './ui/button';
+import LanguageSelection from './LanguageSelection';
 
 const MobileMenu = () => {
   const [model, setModel] = useState(false);
@@ -18,7 +19,8 @@ const MobileMenu = () => {
         <div className='fixed z-50 top-0 right-0 w-3/4 h-screen shadow-lg shadow-sidebar-primary border-sidebar bg-white'>
           <div className='flex flex-col h-full'>
             {/* Close button */}
-            <div className='flex justify-end border-b py-4 px-3'>
+            <div className='flex justify-between border-b py-4 px-3'>
+              <LanguageSelection/>
               <IoClose className='size-6 cursor-pointer' onClick={() => setModel(false)} />
             </div>
 
